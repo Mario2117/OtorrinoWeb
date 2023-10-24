e735f1e31807202a2253a2bb5cb98aa67e5ed56e
Mario2117 <mariosgar2117@gmail.com>
2023-10-24 02:07:26 -0700

    primera version de slider

--- src/build/slider.js
+++ src/build/slider.js
@@ -1,10 +1,10 @@
 let sliderContainer = document.getElementById('review-cards');
 let slider = document.getElementById('slider');
 let cards = slider.getElementsByTagName('li');
+let botonPrev = document.getElementById('prevB');
+let botonNext = document.getElementById('nextB');
 
 sliderChildren = [...slider.children];
-
-
 
 let elementsToShow = 3;
 /*  
@@ -13,7 +13,7 @@ if (document.clientWidth > 768) {
 } else {
     elementsToShow = 1;
 }
-*/
+
 
 sliderChildren.slice(-elementsToShow).reverse().forEach(card => {
     slider.insertAdjacentHTML("afterbegin",card.outerHTML);
@@ -22,27 +22,41 @@ sliderChildren.slice(-elementsToShow).reverse().forEach(card => {
 sliderChildren.slice(0,elementsToShow).reverse().forEach(card => {
     slider.insertAdjacentHTML("beforeend",card.outerHTML);
 });
-
+*/
 let sliderContainerWidth = sliderContainer.clientWidth;
 
 let cardWidth = sliderContainerWidth/elementsToShow;
 
 slider.style.width = cards.length*cardWidth+'px';
+/*  
+botonPrev.onclick = () => {
+    console.log("left");
+    console.log(cardWidth);
+    slider.scrollLeft += cardWidth;
+};
 
+botonNext.onclick = () => {
+    console.log("right");
+    console.log(cardWidth);
+    slider.scrollRight -= cardWidth;
+};
+*/
 for (let index = 0; index < cards.length; index++) {
     const element = cards[index];
     element.style.width = cardWidth+'px';
     
 }
 
-function prev() {
-    console.log(cardWidth);
-    slider.style.marginLeft = (((+slider.style.marginLeft.slice(0,-2))+cardWidth))+'px';
+function next() {
+    console.log(Math.round(+slider.style.marginLeft.slice(0,-2)));
+    if(Math.round(+slider.style.marginLeft.slice(0,-2))!=-Math.round(cardWidth*(cards.length-elementsToShow)))
+        slider.style.marginLeft = (((+slider.style.marginLeft.slice(0,-2))-cardWidth))+'px';
 }
 
-function next() {
-    console.log(cardWidth);
-    slider.style.marginLeft = (((+slider.style.marginLeft.slice(0,-2))-cardWidth))+'px';
+function prev() {
+    console.log(Math.round(+slider.style.marginLeft.slice(0,-2)));
+    if(Math.round(+slider.style.marginLeft.slice(0,-2))!= 0)
+        slider.style.marginLeft = (((+slider.style.marginLeft.slice(0,-2))+cardWidth))+'px';
 }
 
 
