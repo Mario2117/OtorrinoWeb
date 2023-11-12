8e446e9af100276d63ec3baedb8c0575397f0278
Juan Barrera <this.is.juanb@gmail.com>
2023-11-11 23:33:18 -0800

    toggle menu fixes

--- src/build/services.js
+++ src/build/services.js
@@ -18,7 +18,7 @@ let bottomLine = document.getElementById("bottom-line");
 
 
 function serviceMenu(service){
-    if(service == "ear"){
+    if(service == "ear" && !earBox.classList.contains("border-[#303A2C]")){
         bottomLine.classList.remove("mt-5");
         earBox.classList.add("border-8");
         earBox.classList.add("border-[#303A2C]");
@@ -35,8 +35,17 @@ function serviceMenu(service){
         earService.classList.remove("hidden");
         throatService.classList.add("hidden");
         noseService.classList.add("hidden");
+
+    } else if(service == "ear" && earBox.classList.contains("border-[#303A2C]")){
+        earService.classList.add("hidden");
+        earBox.classList.remove("border-[#303A2C]");
+        earBox.classList.remove("border-8");
+        earLine.classList.add("hidden");
+        bottomLine.classList.remove("hidden");
+        bottomLine.classList.add("mt-5");
     }
-    if(service == "throat"){
+
+    if(service == "throat" && !throatBox.classList.contains("border-[#303A2C]")){
         bottomLine.classList.remove("mt-5");
         throatBox.classList.add("border-8");
         throatBox.classList.add("border-[#303A2C]");
@@ -54,8 +63,17 @@ function serviceMenu(service){
         throatService.classList.remove("hidden");
         noseService.classList.add("hidden");
         earService.classList.add("hidden");
+        
+    }else if(service == "throat" && throatBox.classList.contains("border-[#303A2C]")){
+        throatService.classList.add("hidden");
+        throatBox.classList.remove("border-[#303A2C]");
+        throatBox.classList.remove("border-8");
+        throatLine.classList.add("hidden");
+        bottomLine.classList.remove("hidden");
+        bottomLine.classList.add("mt-5");
     }
-    if(service == "nose"){
+
+    if(service == "nose" && !noseBox.classList.contains("border-[#303A2C]")){
         bottomLine.classList.remove("mt-5");
         noseBox.classList.add("border-8");
         noseBox.classList.add("border-[#303A2C]");
@@ -72,65 +90,37 @@ function serviceMenu(service){
         noseService.classList.remove("hidden");
         throatService.classList.add("hidden");
         earService.classList.add("hidden");
+
+    }else if(service == "nose" && noseBox.classList.contains("border-[#303A2C]")){
+        noseService.classList.add("hidden");
+        noseBox.classList.remove("border-[#303A2C]");
+        noseBox.classList.remove("border-8");
+        noseLine.classList.add("hidden");
+        bottomLine.classList.remove("hidden");
+        bottomLine.classList.add("mt-5");
     }
         
 }
 
 
-function myServiceSelector(serviceNum){
-    var openButton = document.getElementById(`open${serviceNum}`);
-    var dialog = document.getElementById(`dialog${serviceNum}`);
-    var closeButton = document.getElementById(`close${serviceNum}`);
+function myServiceSelector(serviceId, operation){
+    var openButton = document.getElementById(`open-${serviceId}`);
+    var dialog = document.getElementById(`dialog-${serviceId}`);
+    var closeButton = document.getElementById(`close-${serviceId}`);
     var overlay = document.getElementById('overlay');
     
     // show the overlay and the dialog
-    openButton.addEventListener('click', function () {
+    if (operation == 'open'){
         dialog.classList.remove('hidden');
         overlay.classList.remove('hidden');
-    });
-    
-    // hide the overlay and the dialog
-    closeButton.addEventListener('click', function () {
+    }
+    else if (operation == 'close'){
         dialog.classList.add('hidden');
         overlay.classList.add('hidden');
-    });
+    }
+    
     
 
 }
 
-
-// // Dialog (needs to optimize)
-// var openButton = document.getElementById('open');
-// var dialog = document.getElementById('dialog');
-// var closeButton = document.getElementById('close');
-// var overlay = document.getElementById('overlay');
-
-// // show the overlay and the dialog
-// openButton.addEventListener('click', function () {
-//     dialog.classList.remove('hidden');
-//     overlay.classList.remove('hidden');
-// });
-
-// // hide the overlay and the dialog
-// closeButton.addEventListener('click', function () {
-//     dialog.classList.add('hidden');
-//     overlay.classList.add('hidden');
-// });
-
-// var openButton2 = document.getElementById('open2');
-// var dialog2 = document.getElementById('dialog2');
-// var closeButton2 = document.getElementById('close2');
-// var overlay2 = document.getElementById('overlay2');
-
-// // show the overlay and the dialog
-// openButton2.addEventListener('click', function () {
-//     dialog2.classList.remove('hidden');
-//     overlay2.classList.remove('hidden');
-// });
-
-// // hide the overlay and the dialog
-// closeButton2.addEventListener('click', function () {
-//     dialog2.classList.add('hidden');
-//     overlay2.classList.add('hidden');
-// });
 
