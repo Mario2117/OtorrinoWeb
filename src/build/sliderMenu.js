d0bc6211076eac2b1910114c8376435c7d1af572
Juan Barrera <this.is.juanb@gmail.com>
2024-01-09 00:10:18 -0800

    arrow fix sliderMenu services&index; autoselect ft

--- src/build/sliderMenu.js
+++ src/build/sliderMenu.js
@@ -22,23 +22,27 @@ function main() {
 
     //Dragging
     let isDraggingRev = false, startX, startScrollLeft, timeOutId;
-    console.log(servicesToShow);
+    // console.log(servicesToShow);
+
     if(servicesToShow < 2) {
-        console.log(servicesToShow);
-
+        // console.log(servicesToShow);
+        let sizeServiceMenu = serviceCards.scrollWidth - serviceCards.offsetWidth;
+        let indexService = 'ear';
         const infiniteScroll=() => {
-
             if (serviceCards.scrollLeft === 0) {
-                // prevservices.classList.add("invisible");
-                // nextservices.classList.remove("invisible");
+                prevservices.classList.add("invisible");
+                nextservices.classList.remove("invisible");
+                indexService = 'ear';
                 
-            } else if(Math.ceil(serviceCards.scrollLeft) === serviceCards.scrollWidth - serviceCards.offsetWidth){
-                // nextservices.classList.add("invisible");
-                // prevservices.classList.remove("invisible");
-            } 
-            else if(Math.ceil(serviceCards.scrollLeft) === (serviceCards.scrollWidth - serviceCards.offsetWidth)/2){
-                // nextservices.classList.remove("invisible");
-                // prevservices.classList.remove("invisible");
+            } else if(Math.ceil(serviceCards.scrollLeft) === sizeServiceMenu){
+                nextservices.classList.add("invisible");      
+                prevservices.classList.remove("invisible");
+                indexService = 'nose';
+                
+            } else {
+                nextservices.classList.remove("invisible");
+                prevservices.classList.remove("invisible");
+                indexService = 'throat';
             }
 
         };
@@ -50,14 +54,27 @@ function main() {
             serviceCards.classList.add("scroll-smooth");
             serviceCards.scrollLeft -= serviceWidth;
             serviceCards.classList.remove("scroll-smooth");
+            if (indexService === 'nose') {
+                serviceMenu('throat');
+            }
+            else if (indexService === 'throat') {
+                serviceMenu('ear');
+            }
+            
         };
 
         nextservices.onclick = () => {
             serviceCards.classList.add("scroll-smooth");
             serviceCards.scrollLeft += serviceWidth;
             serviceCards.classList.remove("scroll-smooth");
+            if (indexService === 'ear') {
+                serviceMenu('throat');
+            }
+            else if (indexService === 'throat') {
+                serviceMenu('nose');
+            }
         };
-
+        
         //endButtonMovement
 
     } else {}
@@ -70,3 +87,4 @@ window.addEventListener("resize", function () {
     inicializaVariables();
     main();
 });
+
